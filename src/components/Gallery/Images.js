bd9b4ab269eb04d23924d0732c8dd72ce7719da3
Kishore Mongar <84139548+fushigina-Shinobi@users.noreply.github.com>
2022-08-14 12:47:11 +0530

    gallery added

---
+++ src/components/Gallery/Images.js
@@ -0,0 +1,198 @@
+const Images = [
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1654791402/Trail%20Addek%20Slides/PSX_20191225_205212_ssxzdz.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1654791402/Trail%20Addek%20Slides/PSX_20191225_205212_ssxzdz.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    tags: [
+      { value: "Ocean", title: "Ocean" },
+      { value: "People", title: "People" },
+    ],
+    caption: "After Rain (Jeshu John - designerspics.com)",
+    // customOverlay: "Hello World",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1653818601/Trail%20Addek%20Slides/IMG_20180301_202602_735_rf4qsh.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1653818601/Trail%20Addek%20Slides/IMG_20180301_202602_735_rf4qsh.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 212,
+    tags: [
+      { value: "Ocean", title: "Ocean" },
+      { value: "People", title: "People" },
+    ],
+    caption: "Boats (Jeshu John - designerspics.com)",
+  },
+
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1653818594/Trail%20Addek%20Slides/PicsArt_05-06-03.35.11_coa0vj.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1653818594/Trail%20Addek%20Slides/PicsArt_05-06-03.35.11_coa0vj.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 212,
+    tags: [
+      { value: "Ocean", title: "Ocean" },
+      { value: "People", title: "People" },
+    ],
+    caption: "Boats (Jeshu John - designerspics.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1653818587/Trail%20Addek%20Slides/PSX_20191225_205952_i1rljc.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1653818587/Trail%20Addek%20Slides/PSX_20191225_205952_i1rljc.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    caption: "8H (gratisography.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1653818572/Trail%20Addek%20Slides/PSX_20191225_171616_mvwgd3.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1653818572/Trail%20Addek%20Slides/PSX_20191225_171616_mvwgd3.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 190,
+    caption: "286H (gratisography.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459972/Trail%20Addek%20Slides/background-image-1_ztqmro.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459972/Trail%20Addek%20Slides/background-image-1_ztqmro.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 148,
+    caption: "315H (gratisography.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459830/Trail%20Addek%20Slides/PSX_20191225_210138_hpc5ye.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459830/Trail%20Addek%20Slides/PSX_20191225_210138_hpc5ye.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 213,
+    caption: "201H (gratisography.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459830/Trail%20Addek%20Slides/PSX_20191029_143043_khmsui.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459830/Trail%20Addek%20Slides/PSX_20191029_143043_khmsui.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    caption: "Big Ben (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459829/Trail%20Addek%20Slides/PSX_20210201_105026_u7myzl.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459829/Trail%20Addek%20Slides/PSX_20210201_105026_u7myzl.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 113,
+    caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459828/Trail%20Addek%20Slides/PSX_20181029_180810_a6d93d.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459828/Trail%20Addek%20Slides/PSX_20181029_180810_a6d93d.jpg",
+    thumbnailWidth: 313,
+    thumbnailHeight: 320,
+    caption: "Wood Glass (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459829/Trail%20Addek%20Slides/PSX_20181129_122704_u9px3m.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459829/Trail%20Addek%20Slides/PSX_20181129_122704_u9px3m.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 213,
+    caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459828/Trail%20Addek%20Slides/IMG_20200811_155356_969_tm4dvx.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459828/Trail%20Addek%20Slides/IMG_20200811_155356_969_tm4dvx.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 213,
+    caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459828/Trail%20Addek%20Slides/PicsArt_01-28-09.23.13_pjpdgr.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459828/Trail%20Addek%20Slides/PicsArt_01-28-09.23.13_pjpdgr.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 213,
+    caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459829/Trail%20Addek%20Slides/PSX_20181030_070205_qozd2t.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459829/Trail%20Addek%20Slides/PSX_20181030_070205_qozd2t.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 213,
+    caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459827/Trail%20Addek%20Slides/Teesta_River_rkfvzj.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459827/Trail%20Addek%20Slides/Teesta_River_rkfvzj.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 113,
+    caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459827/Trail%20Addek%20Slides/IMG_20190310_174112_683_shw1wk.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459827/Trail%20Addek%20Slides/IMG_20190310_174112_683_shw1wk.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 113,
+    caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459828/Trail%20Addek%20Slides/LRM_EXPORT_63428819537990_20181106_194205797_rrlyif.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459828/Trail%20Addek%20Slides/LRM_EXPORT_63428819537990_20181106_194205797_rrlyif.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 113,
+    caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459826/Trail%20Addek%20Slides/IMG_20180308_134719_199_vjf38q.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459826/Trail%20Addek%20Slides/IMG_20180308_134719_199_vjf38q.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 113,
+    caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459827/Trail%20Addek%20Slides/PSX_20200518_195716_fyic8x.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459827/Trail%20Addek%20Slides/PSX_20200518_195716_fyic8x.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 212,
+    tags: [
+      { value: "Ocean", title: "Ocean" },
+      { value: "People", title: "People" },
+    ],
+    caption: "Boats (Jeshu John - designerspics.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459827/Trail%20Addek%20Slides/PSX_20201114_210439_qcwvh7.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459827/Trail%20Addek%20Slides/PSX_20201114_210439_qcwvh7.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 212,
+    tags: [
+      { value: "Ocean", title: "Ocean" },
+      { value: "People", title: "People" },
+    ],
+    caption: "Boats (Jeshu John - designerspics.com)",
+  },
+  {
+    src: "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459826/Trail%20Addek%20Slides/IMG_20180313_103124_721_xvidri.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1660459826/Trail%20Addek%20Slides/IMG_20180313_103124_721_xvidri.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 212,
+    tags: [
+      { value: "Ocean", title: "Ocean" },
+      { value: "People", title: "People" },
+    ],
+    caption: "Boats (Jeshu John - designerspics.com)",
+  },
+];
+
+export default Images;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,17 +1,16 @@
 import React from "react";
-import "./About.css";
+import "./Gallery.css";
+import Images from "../components/Gallery/Images";
+import Gallery from "react-grid-gallery";
 
 const About = () => {
   return (
-    <section className="section about-section">
-      <h1 className="section-title">about us</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, sed
-        accusantium deserunt dolor officia autem rem quo natus, ducimus
-        asperiores obcaecati, voluptatibus accusamus. Sit aspernatur assumenda
-        voluptatum ipsa beatae ducimus unde pariatur numquam optio perspiciatis
-        eum tempora quisquam, dolore qui!
-      </p>
+    <section className="gallery-container">
+      <Gallery
+        images={Images}
+        // showCloseButton={false}
+        backdropClosesModal={true}
+      />
     </section>
   );
 };
